e7b8e32d078ce486b85eb6ef6b0aebcba084d573
Arild Matsson <arild.matsson@gu.se>
2023-01-18 18:14:24 +0100

    Fix WP_CONTENT_URL

--- wp-config.php
+++ wp-config.php
@@ -2,15 +2,31 @@
 $table_prefix = 'wpql_';
 
 // Keep plugin/theme code dir outside WP core.
-define( 'WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/wp-content' );
+define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/wp-content');
 
-if ( file_exists( $_SERVER['DOCUMENT_ROOT'] . '/wp-config-local.php' ) ) {
-  require( $_SERVER['DOCUMENT_ROOT'] . '/wp-config-local.php' );
+// Load local-environment settings: database, url etc.
+if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/wp-config-local.php')) {
+  require($_SERVER['DOCUMENT_ROOT'] . '/wp-config-local.php');
 }
 
+// Finish setting variables after loading local settings.
+if (!defined('WP_HOME')) {
+  if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on') {
+    define('WP_HOME', 'http://' . $_SERVER['SERVER_NAME']);
+  }
+  else {
+    define('WP_HOME', 'https://' . $_SERVER['SERVER_NAME']);
+  }
+}
+if (!defined('WP_SITEURL')) {
+  define('WP_SITEURL', WP_HOME . '/wp');
+}
+
+define('WP_CONTENT_URL', WP_HOME . '/wp-content');
+
 /** Absolute path to the WordPress directory. */
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
+if (!defined('ABSPATH')) {
+  define('ABSPATH', __DIR__ . '/');
 }
 
 /** Sets up WordPress vars and included files. */
