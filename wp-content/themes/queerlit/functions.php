ec43cbc3b351a6bc291e6333a419f7c82de2b1ef
Arild Matsson <arild.matsson@gu.se>
2023-01-18 18:14:36 +0100

    Barlow Condensed

--- wp-content/themes/queerlit/functions.php
+++ wp-content/themes/queerlit/functions.php
@@ -1,7 +1,10 @@
 <?php
+/* @see https://developer.wordpress.org/themes/advanced-topics/child-themes/ */
 
 add_action('wp_enqueue_scripts', 'queerlit_enqueue_styles');
 function queerlit_enqueue_styles() {
   wp_enqueue_style('queerlit-style', get_stylesheet_uri(),
     ['twenty-twenty-one-style'], wp_get_theme()->get('Version'));
+
+  wp_enqueue_style( 'queerlit-external', 'https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@200&display=swap', false );
 }
