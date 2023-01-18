d4a730ccc0bd1ca338550e1eef337e939447818f
Arild Matsson <arild.matsson@gu.se>
2023-01-18 18:44:53 +0100

    Custom styles to menu

--- wp-content/themes/queerlit/functions.php
+++ wp-content/themes/queerlit/functions.php
@@ -6,5 +6,5 @@ function queerlit_enqueue_styles() {
   wp_enqueue_style('queerlit-style', get_stylesheet_uri(),
     ['twenty-twenty-one-style'], wp_get_theme()->get('Version'));
 
-  wp_enqueue_style( 'queerlit-external', 'https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@200&display=swap', false );
+  wp_enqueue_style( 'queerlit-external', 'https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@300&display=swap', false );
 }
