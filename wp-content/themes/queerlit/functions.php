755cca6290f5030c04d1771c7107b4748a3da6de
Arild Matsson <arild.matsson@gu.se>
2023-02-14 14:59:03 +0100

    Logo link always

--- wp-content/themes/queerlit/functions.php
+++ wp-content/themes/queerlit/functions.php
@@ -14,5 +14,8 @@ add_filter('get_custom_logo', 'queerlit_get_custom_logo');
 function queerlit_get_custom_logo($html) {
   $home_url = 'https://queerlit.dh.gu.se/';
   $html = preg_replace('/href="[^"]*"/', 'href="' . $home_url . '"', $html);
+  if (strpos($html, 'href=') === FALSE) {
+    $html = preg_replace('/<img [^>]*\/>/', '<a href="' . $home_url . '">\0</a>', $html);
+  }
   return $html;
 }
